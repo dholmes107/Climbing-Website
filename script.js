a23c36b7f255032cf61d6aef1d9565021c4c7d5e
dholmes107 <103646980+dholmes107@users.noreply.github.com>
2022-06-09 21:03:24 +0000

    - added buttons to bouldering.html - removed title tags from .html pages -started working on JS function to show and hide text

---
+++ script.js
@@ -0,0 +1,7 @@
+//Building OnClick text for bouldering.html butttons: https://www.w3schools.com/jsref/event_onclick.asp reference site later//
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+
+//Building OnClick text for bouldering.html butttons: https://www.w3schools.com/jsref/event_onclick.asp reference site later//
