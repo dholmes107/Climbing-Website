0591bf03f9787707a85391fc5379cbadf2b3c725
dholmes107 <103646980+dholmes107@users.noreply.github.com>
2022-06-11 00:20:10 +0000

    rewrote boulering.html page to include onclick location.href links to three YouTube videos on climbing

--- script.js
+++
@@ -1,7 +0,0 @@
-//Building OnClick text for bouldering.html butttons: https://www.w3schools.com/jsref/event_onclick.asp reference site later//
-
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
-}
-
-//Building OnClick text for bouldering.html butttons: https://www.w3schools.com/jsref/event_onclick.asp reference site later//
