e31dba10ccf7c8a85cedae543d641d784f62ac39
dholmes107 <103646980+dholmes107@users.noreply.github.com>
2022-07-06 02:27:27 +0000

    -updated holds.html to include hover animations to each photo.
    - removed test.html document.
    
    -Updated comments within holds.html and style.css

--- script.js
+++ script.js
@@ -9,19 +9,19 @@
 
 
 //Capture DOM elements//
-const ladderHold = document.querySelector(".ladderHold");
-const jugHold = document.querySelector(".jugHold");
-const crimpHold = document.querySelector(".crimpHold");
-const pinchHold = document.querySelector(".pinchHold");
-const pocketHold = document.querySelector(".pocketHold");
-const sloperHold = document.querySelector(".sloperHold");
+// const ladderHold = document.querySelector(".ladderHold");
+// const jugHold = document.querySelector(".jugHold");
+// const crimpHold = document.querySelector(".crimpHold");
+// const pinchHold = document.querySelector(".pinchHold");
+// const pocketHold = document.querySelector(".pocketHold");
+// const sloperHold = document.querySelector(".sloperHold");
 //Capture DOM elements//
 
 
 
 
-function findMyClimbingHold(){
+// function findMyClimbingHold(){
 
 
 
-}
+// }
