37307642d5f387bbc3ded02524425a0cdfd3bb66
dholmes107 <103646980+dholmes107@users.noreply.github.com>
2022-07-08 20:08:54 +0000

    - added climb.html page.
    - embedded YouTube video.
    
    - working on climb.html page positioning.

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// 1. Create Onclick function with If/Else statements.//
-
-//2. If image is selected. Flip/remove photo and display text of image.//
-
-//3. Create #id tags for each img on holds.html. Then associate those Ids in order to build the if/else statements. So that the function can understand when each image is clicked it will perform a different action.//
-
-//4. Refer to: w3schools.com/howto/howto_css_flip_image.asp
-// Might be possible to flip without JS? Or use function still but follow 3D Flip Image with Text example in link.
-
-
 //Capture DOM elements//
 // const ladderHold = document.querySelector(".ladderHold");
 // const jugHold = document.querySelector(".jugHold");
